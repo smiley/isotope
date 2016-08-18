c8d0f9087520989f66f145094d66e9932a057e9d
Smiley Barry <smiley@users.noreply.github.com>
2016-08-18 16:21:45 +0300

    Fixed a bug where Isotope would accidentally use the 32-bit version of common controls in both architectures.

--- isotope/main.cpp
+++ isotope/main.cpp
@@ -2,7 +2,7 @@
 
 #include <Commctrl.h>
 #pragma comment(lib, "comctl32.lib")
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #include <Shlwapi.h>
 #pragma comment(lib, "Shlwapi.lib")
